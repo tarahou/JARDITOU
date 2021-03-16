d091b2f993481d46249e651de5281a471c7df4ee
tarahou <79140037+tarahou@users.noreply.github.com>
2021-03-16 16:13:02 +0100

    Delete JAVASCRIPT directory

--- JAVASCRIPT/javascript-boucles4.js
+++
@@ -1,8 +0,0 @@
-var X = parseInt(window.prompt('Saisissez un nombre :'));
-var N = parseInt(window.prompt('Saisissez le nombre de multiple :'));
-var total = 0;
-
-for (i = 0; i <= N; i++) {
-    total = i * X;
-}
-alert(X + ' x ' + (i - 1) + ' = ' + total);
