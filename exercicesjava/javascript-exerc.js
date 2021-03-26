9ff4bd3a36564fe1a7c7407a6b61973ea2db55b2
tarahou <79140037+tarahou@users.noreply.github.com>
2021-03-26 11:20:58 +0100

    Delete exercicesjava directory

--- exercicesjava/javascript-exerc.js
+++
@@ -1,3 +0,0 @@
-var name = "ahlam";
-var number = 7;
-prompt("Bonjour mon prénom est" + " " + name + " " + "et" + " " + "mon chiffre préféré est" + " " + number);
--- exercicesjava/javascript1.js
+++
@@ -1,5 +0,0 @@
-var nb = window.prompt("Saisissez un nombre");
-if (nb % 2 == 0)
-    alert("Ce nombre est pair");
-else
-    alert("Ce nombre est impair");
